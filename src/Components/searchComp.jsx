cbdd7575e3e3b2ae4cfc845595c0a32f4eb8349d
Nikita Sobolev <NIkitanrf81@gmail.com>
2025-02-01 22:39:36 +0700

    feat: artists search label added

--- src/Components/searchComp.jsx
+++ src/Components/searchComp.jsx
@@ -12,8 +12,11 @@ export default class SearchComp extends React.Component{
             isOpen: false,
         };
         this.getSearch = this.getSearch.bind(this);
+        this.getSearchArtists = this.getSearchArtists.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleSearchArtists = this.handleSearchArtists.bind(this);
         this.renderSearch = this.renderSearch.bind(this);
+        this.renderSearchArtists = this.renderSearchArtists.bind(this);
     }
 
     getSearch(search){
@@ -31,10 +34,34 @@ export default class SearchComp extends React.Component{
             console.error(err)
         }
     }
+
+    getSearchArtists(search){
+        const url = `https://deezerdevs-deezer.p.rapidapi.com/artist/${search}`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-key': '1683d62ab0msh32b82388bbf2e0ap1e8162jsn839af447016e',
+                'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com'
+            }
+        };
+        try{
+            fetch(url, options).then(data => data.json()).then(json => this.handleSearchArtists(json));
+        } catch (err){
+            console.error(err)
+        }
+    }
+
     handleSearch(json){
         console.log(json.data);
         this.setState({
             search_results: json.data,
+        })
+    }
+
+    
+    handleSearchArtists(json){
+        this.setState({
+            search_results: json,
         })
     }
 
@@ -44,23 +71,51 @@ export default class SearchComp extends React.Component{
         }
     }
 
+    renderSearchArtists(){
+        console.log(this.state.search_results);
+        if (this.state.search_results){
+            return <div className="search-list-item" key={this.state.search_results.id}>{this.state.search_results.name} <img className="search-list-item-img-artists"src={this.state.search_results.picture_big}></img></div>
+        }
+    }
+
+
 
     render(){
-        return(
-            <div className="search-cont">
-                <div className="search-modal-blockscreen none" onClick={(e) => {e.target.classList.toggle("none"); document.querySelector(".search-modal").classList.toggle("none")}}></div>
-                <form  className="search-modal-form"onSubmit={(e) => {this.getSearch(this.state.value); e.preventDefault(); document.querySelector(".search-modal").classList.toggle("none"); document.querySelector(".search-modal-blockscreen").classList.toggle("none")}}>
-                    <input placeholder="type here..." value={this.state.value} onChange={(e) =>  this.setState({value: e.target.value})}></input>
-                    <button type="submit"> search </button>
-                    <div className="search-list">
-                    </div>
-                    <div className="search-modal none">SEARCH RESULTS
-                        {this.renderSearch()}
-                    </div>
-                </form>
-
-               
-            </div>
-        )
+        if (this.props.isArtists){
+            return(
+                <div className="search-cont">
+                    <div className="search-modal-blockscreen none" onClick={(e) => {e.target.classList.toggle("none"); document.querySelector(".search-modal-artists").classList.toggle("none")}}></div>
+                    <form  className="search-modal-form"onSubmit={(e) => {this.getSearchArtists(this.state.value); e.preventDefault(); document.querySelector(".search-modal-artists").classList.toggle("none"); document.querySelector(".search-modal-blockscreen").classList.toggle("none")}}>
+                        <input placeholder="type here..." value={this.state.value} onChange={(e) =>  this.setState({value: e.target.value})}></input>
+                        <button type="submit"> search </button>
+                        <div className="search-list">
+                        </div>
+                        <div className="search-modal-artists none">SEARCH ARTISTS
+                            {this.renderSearchArtists()}
+                        </div>
+                    </form>
+    
+                   
+                HI</div>
+            )
+        }
+        else{
+            return(
+                <div className="search-cont">
+                    <div className="search-modal-blockscreen none" onClick={(e) => {e.target.classList.toggle("none"); document.querySelector(".search-modal").classList.toggle("none")}}></div>
+                    <form  className="search-modal-form"onSubmit={(e) => {this.getSearch(this.state.value); e.preventDefault(); document.querySelector(".search-modal").classList.toggle("none"); document.querySelector(".search-modal-blockscreen").classList.toggle("none")}}>
+                        <input placeholder="type here..." value={this.state.value} onChange={(e) =>  this.setState({value: e.target.value})}></input>
+                        <button type="submit"> search </button>
+                        <div className="search-list">
+                        </div>
+                        <div className="search-modal none">SEARCH RESULTS
+                            {this.renderSearch()}
+                        </div>
+                    </form>
+    
+                   
+                </div>
+            )
+        }
     }
 }
--- src/Screens/artists.jsx
+++ src/Screens/artists.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./artists.css"
 import SidebarFull from "../Components/sidebarFull";
 import Playbar from "../Components/playbar";
+import SearchComp from "../Components/searchComp";
 
 export default function Artists(){
     return(
         <div className="cont-artists">
             <Playbar/>
             <SidebarFull/>
-            HI ARTISTS
+            <SearchComp isArtists={true}/>
         </div>
     )
 }
